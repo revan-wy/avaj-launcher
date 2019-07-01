e81982c69a803216de03cb3a1abe4463cbf45634
Renzo VAN WYK <revan-wy@c2r6s4.wethinkcode.co.za>
2019-07-01 18:24:08 +0200

    started adding skeleton to Balloon

---
+++ src/avaj/simulator/vehicles/Balloon.java
@@ -0,0 +1,9 @@
+package avaj.simulator.vehicles;
+
+public class Balloon extends Aircraft implements Flyable {
+    private WeatherTower weatherTower;
+
+    package Balloon(String name, Coordinates coordinates) {
+
+    }
+}
