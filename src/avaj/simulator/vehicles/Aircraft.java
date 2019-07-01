101a072fe28248b4a1d860e55a0b2b403b1053db
Renzo VAN WYK <revan-wy@c2r6s4.wethinkcode.co.za>
2019-07-01 18:02:10 +0200

    added skeleton for aircraft class

---
+++ src/avaj/simulator/vehicles/Aircraft.java
@@ -0,0 +1,17 @@
+package avaj.simulator.vehicles;
+
+public class Aircraft {
+    protected long id;
+    protected String name;
+    protected Coordinates coordinates;
+
+    private static long idCounter;
+
+    protected Aircraft(String name, Coordinates coordinates) {
+        
+    }
+
+    private long nextId() {
+        
+    }
+}
