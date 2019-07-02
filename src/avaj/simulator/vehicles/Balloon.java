09dbad7900790fa36ccd29ed5e8ecb4834fd5519
Renzo VAN WYK <revan-wy@c2r6s6.wethinkcode.co.za>
2019-07-02 16:12:17 +0200

    expanded implementation of Balloon

--- src/avaj/simulator/vehicles/Balloon.java
+++ src/avaj/simulator/vehicles/Balloon.java
@@ -1,9 +1,20 @@
 package avaj.simulator.vehicles;
 
+import avaj.simulator.*;
+import avaj.weather.*;
+
 public class Balloon extends Aircraft implements Flyable {
     private WeatherTower weatherTower;
 
-    package Balloon(String name, Coordinates coordinates) {
-
+    Balloon(String name, Coordinates coordinates) {
+        super(name, coordinates);
     }
+
+    public void updateConditions() {
+		
+	}
+
+    public void registerTower(WeatherTower weatherTower) {
+		
+	}
 }
