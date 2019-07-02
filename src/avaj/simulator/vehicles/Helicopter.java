1762c893bf096bb8c74fcc771d85acb8d58a8d2b
Renzo VAN WYK <revan-wy@c2r6s6.wethinkcode.co.za>
2019-07-02 16:15:09 +0200

    fixed Helicopter constructor

--- src/avaj/simulator/vehicles/Helicopter.java
+++ src/avaj/simulator/vehicles/Helicopter.java
@@ -8,7 +8,7 @@ public class Helicopter extends Aircraft implements Flyable {
     private WeatherTower weatherTower;
 
     Helicopter(String name, Coordinates coordinates) {
-
+        super(name, coordinates);
     }
 
     public void updateConditions() {
